72883f363cd2d2baab5a276ca89a718a1874539f
Adrian <adrianlam1511@gmail.com>
2023-08-13 20:48:20 -0700

    Added black formatter and linters for python, added copilot

--- config.lua
+++ config.lua
@@ -25,4 +25,75 @@ vim.g.clipboard = {
   },
 }
 
+-- lvim.format_on_save.enabled = true
+
 lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.py" }
+
+-- Formatters
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = {
+      "typescript",
+      "typescriptreact"
+    },
+  },
+  {
+    name = "black"
+  },
+}
+
+-- Linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", args = { "--ignore=E203"}, filetypes = { "python" }}}
+
+-- Env switcher
+lvim.plugins = {
+  "ChristianChiarulli/swenv.nvim",
+  "stevearc/dressing.nvim",
+}
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Python",
+  c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+}
+
+-- For tailwindcss intellisense
+require("lvim.lsp.manager").setup "tailwindcss"
+
+-- Github Copilot
+lvim.plugins = {  
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
+
+local ok, copilot = pcall(require, "copilot")
+if not ok then
+  return
+end
+
+copilot.setup {
+  suggestion = {
+    keymap = {
+      accept = "<c-l>",
+      next = "<c-j>",
+      prev = "<c-k>",
+      dismiss = "<c-h>",
+    },
+  },
+}
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
